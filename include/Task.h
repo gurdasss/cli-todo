27c96af5d59f9d4ddc8ccb0ecd348b079d85fcc9
Gurdas Satwani <gurdassatwani1@gmail.com>
2025-06-26 17:33:30 +0530

    fixed ID overflow and input validation

--- include/Task.h
+++ include/Task.h
@@ -1,6 +1,7 @@
 #include <cstdint>     // for std::int16
 #include <string>      // for std::string
 #include <string_view> // for std::string_view
+#include <cassert>     // for assert
 
 #ifndef TASK_H
 #define TASK_H
@@ -8,11 +9,6 @@
 class Task
 {
 public:
-    // the user should be able to set (or reset)
-    // task count to begin ID sequencing from
-    // arbitrary number.
-    static std::int16_t s_taskCount;
-
     // Status is a member type
     enum Status
     {
@@ -28,8 +24,18 @@ public:
           m_status{status},
           m_category{category}
     {
+        // make sure that each Task object ID's are non-negative
+        // NOTE: if s_taskCount = std::numeric_limits<std::int16_t>::max() + 1
+        // then we can't construct anymore Task objects
+        // because we're assuming that each Task object will
+        // get a unique sequential non-negative integer value as its ID
+        // we might face integer overflow
+        // can better handle this situation via try-and-catch
+        assert(m_id >= 0 && "Can't construct anymore Task object");
     }
 
+    static bool setIDCount(std::int16_t initialIDCount);
+    static std::int16_t getIDCount() { return s_taskCount; }
     std::int16_t getID() const { return m_id; }
 
     void setTitle(std::string_view title) { m_title = title; }
@@ -42,6 +48,14 @@ public:
     const std::string &getCategory() const { return m_category; }
 
 private:
+    // applied inline to avoid violating ODR
+
+    // the user should be able to set (or reset)
+    // task count to begin ID sequencing from
+    // arbitrary number within the range
+    // 0 >= && <= (2^16 - 1)
+    // ID generator
+    static inline std::int16_t s_taskCount{};
     std::int16_t m_id{};
     std::string m_title{};
     Status m_status{};
@@ -50,7 +64,17 @@ private:
 
 // applied inline to avoid violating ODR
 
-// ID generator
-inline std::int16_t Task::s_taskCount{};
+// returns true, if ID count was set successfully
+inline bool Task::setIDCount(std::int16_t initialIDCount)
+{
+    // we need ID count to be non-negative
+    if (initialIDCount < 0)
+        return false;
+
+    // 0 >= initialIDCount && initialIDCount <= std::numeric_limits<int16_t>::max()
+
+    s_taskCount = initialIDCount;
+    return true;
+}
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,21 @@
 #include "Task.h"
 #include <iostream>
-#include <string> // for std::string
+#include <string>    // for std::string
+#include <limits>    // for std::numeric_limits
+#include <algorithm> // for std::transform
+#include <cctype>    // for std::tolower
+#include <optional>  // for std::optional
+
+void ignoreLine();
+bool hasUnextractedInput();
+
+std::istream &operator>>(std::istream &in, Task::Status &status);
+std::ostream &operator<<(std::ostream &out, Task::Status status);
+
+void toLowerCase(std::string &string);
+
+std::optional<Task::Status> getStatusFromChar(char statusChar);
+std::string_view getStatusString(Task::Status status);
 
 int main()
 {
@@ -11,20 +26,71 @@ int main()
         std::string command{};
         std::cin >> command;
 
-        // TODO: NEED TO HANDLE EOF CHARACTERS
-
-        if (command == "add")
+        // Check for failed extraction
+        // If the previous extraction failed
+        if (!std::cin)
         {
-            // once we got the inputs from add command
-            // in the input buffer
-            // we need to extract each one of them individually
+            std::cerr << "std::cin is in failed state.\n";
+            if (std::cin.eof()) // If the user entered an EOF
+                std::exit(0);   // Shut down the program now
+        }
+
+        // convert the recent extracted input from the input buffer
+        // to lower case
+        toLowerCase(command);
+
+        // WARNING: the user might add trailing whitespace(s) after
+        // a valid input, which will satisfy the below condition
+        if (command == "add" && hasUnextractedInput())
+        {
+            // extract and store extra inputs from the input buffer
+            // on each call to std::cin
             std::string title{};
             std::cin >> title;
+
+            // check if, after the last operation
+            // the input buffer still have some
+            // unextracted input left
+            if (!hasUnextractedInput())
+            {
+                std::cout << "add: missing status and category\n";
+                std::cout << "add [title] [status] [category]\n";
+                continue;
+            }
+
+            Task::Status status{};
+            std::cin >> status;
+
+            // if last operation did'nt succeeded
+            if (!std::cin)
+            {
+                std::cout << "Enter a valid status: [P]: Progress, "
+                             "[I]: In Progress\n";
+
+                // clear all the states of std::cin
+                // and remove all the unextracted input
+                // from the input buffer
+                std::cin.clear();
+                ignoreLine();
+                continue;
+            }
+
+            // check if, after the last operation
+            // the input buffer still have some
+            // unextracted input left
+            if (!hasUnextractedInput())
+            {
+                std::cout << "add: missing category\n";
+                std::cout << "add [title] [status] [category]\n";
+                continue;
+            }
 
             std::string category{};
             std::cin >> category;
 
-            std::cout << title << ' ' << category << '\n';
+            std::cout << title << ' '
+                      << status << ' '
+                      << category << '\n';
         }
         else if (command == "exit")
             break;
@@ -32,7 +98,101 @@ int main()
             ;
         else
             std::cout << "invalid command! type help\n";
+
+        // if there's still any unextrated input(s)
+        // left in the input buffer clear all those extra inputs
+        if (hasUnextractedInput())
+            ignoreLine();
     }
 
     return 0;
+}
+
+void ignoreLine()
+{
+    // clear up to the largest value that can be stored
+    // in a variable of type std::streamsize characters out of the buffer,
+    // or until a '\n' character is removed
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+}
+
+// returns true if std::cin has unextracted input on the current line, false otherwise
+bool hasUnextractedInput()
+{
+    return !std::cin.eof() && std::cin.peek() != '\n';
+}
+
+void toLowerCase(std::string &string)
+{
+    std::transform(
+        string.begin(), // start from the first letter
+        string.end(),   // stop at the end
+        string.begin(), // where to put the result (back into the same string)
+
+        // a unary lambda function that will convert an unsigned char
+        // to lower case
+        [](unsigned char c)
+        { return std::tolower(c); } // rule: make lowercase
+    );
+}
+
+std::istream &operator>>(std::istream &in, Task::Status &status)
+{
+    char statusChar{};
+    in >> statusChar;
+
+    statusChar = static_cast<char>(std::tolower(statusChar));
+
+    std::optional<Task::Status> statusOPT{getStatusFromChar(statusChar)};
+
+    if (statusOPT)
+    {
+        // dereference std::optional to get matching enumerator
+        status = *statusOPT;
+        return in;
+    }
+
+    // We didn't find a match, so input must have been invalid
+    // so we will set input stream to fail state
+    in.setstate(std::ios_base::failbit);
+
+    return in;
+}
+
+std::ostream &operator<<(std::ostream &out, Task::Status status)
+{
+    out << getStatusString(status);
+    return out;
+}
+
+std::optional<Task::Status> getStatusFromChar(char statusChar)
+{
+    switch (statusChar)
+    {
+    case 'p':
+        return Task::pending;
+    case 'i':
+        return Task::inProgress;
+    case 'd':
+        return Task::done;
+
+    default:
+        return std::nullopt;
+    }
+}
+
+std::string_view getStatusString(Task::Status status)
+{
+    switch (status)
+    {
+    case Task::pending:
+        return "Pending";
+    case Task::inProgress:
+        return "In-Progress";
+    case Task::done:
+        return "Done";
+
+    default:
+        return "???";
+    }
 }
